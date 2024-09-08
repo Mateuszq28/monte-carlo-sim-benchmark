8f8cff637156afe5d880947c94eb989565da7556
mateusz_pikus <mateuszq28@gmail.com>
2024-09-08 19:27:07 +0200

    fix paths

--- time_wrapper.py
+++ time_wrapper.py
@@ -5,18 +5,25 @@ import time
 sim_names_list = ["tiny", "small", "mc321"]
 sim_name = sim_names_list[0]
 
+# filenames
+sim_exe = "{}_mc.exe".format(sim_name)
+sim_out = "{}_out.txt".format(sim_name)
+sim_c = "{}_mc.c".format(sim_name)
+sim_log = "{}_log.txt".format(sim_name)
+
 # open script location
 self_path = os.path.dirname(os.path.abspath(__file__))
 rel_scritpt_dir = "benchmark_sims"
+# rel_scritpt_dir = "original"
 abs_script = os.path.join(self_path, rel_scritpt_dir)
 os.system("cd {}".format(abs_script))
 
 # do simulation
 start_time = time.time()
 if sim_name != "mc321":
-    os.system("{}_mc.exe > {}_out.txt".format(sim_name, sim_name))
+    os.system("{} > {}".format(sim_exe, sim_out))
 else:
-    os.system("{}_mc.exe".format(sim_name))
+    os.system(sim_exe)
 end_time = time.time()
 
 # print time
@@ -24,22 +31,25 @@ exec_time = end_time - start_time
 time_text = "time elapsed: {0:.4f} seconds".format(exec_time)
 print(time_text)
 
+# sim out files - with paths to use in python
+path_sim_out = os.path.join(rel_scritpt_dir, sim_out)
+path_sim_c = os.path.join(rel_scritpt_dir, sim_c)
+path_sim_log = os.path.join(rel_scritpt_dir, sim_log)
+
 # save simulation log
-f = open("{}_log.txt", "w")
+f = open(path_sim_log, "w")
 f.write(time_text)
 f.write("\n\n")
-f.wite("sim results:")
+f.write("sim results:")
 f.write("\n\n")
-with open("{}_out.txt".format(sim_name), "r") as f_out:
+with open(path_sim_out, "r") as f_out:
     f_out_text = f_out.read()
     f.write(f_out_text)
 f.write("\n\n")
 f.write("code that generated this simulation:")
 f.write("\n\n")
-with open("{}_mc.c".format(sim_name), "r") as f_code:
+with open(path_sim_c, "r") as f_code:
     f_code_text = f_code.read()
     f.write(f_code_text)
 f.close()
-
-
 
