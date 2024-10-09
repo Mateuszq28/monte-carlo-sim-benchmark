be33eaa96157fe6001efd6635de1b4f07194c894
mateusz_pikus <mateuszq28@gmail.com>
2024-10-09 16:26:01 +0200

    demo done
    grid
    
    duzo testow
    
    wyniki
    
    usunieto wyniki compare 2
    
    przed tescikiem malym
    
    biblio
    
    bib pisanie

--- compare_cubes/vispy_chart3d.py
+++ compare_cubes/vispy_chart3d.py
@@ -22,14 +22,14 @@ def chart3d_vispy(data1=None, data2=None, data3=None, min_heat=None, max_heat=No
         max_heat = np.max([np.max(data1), np.max(data2), np.max(data3)])
 
     # markery na rogach
-    min_marker = min_heat + 0.01 * max_heat
-    max_marker = max_heat
-    data1[0:40, 0:40] = min_marker
-    data1[-40:-1, -40:-1] = max_marker
-    data2[0:40, 0:40] = min_marker
-    data2[-40:-1, -40:-1] = max_marker
-    data3[0:40, 0:40] = min_marker
-    data3[-40:-1, -40:-1] = max_marker
+    # min_marker = min_heat + 0.01 * max_heat
+    # max_marker = max_heat
+    # data1[0:40, 0:40] = min_marker
+    # data1[-40:-1, -40:-1] = max_marker
+    # data2[0:40, 0:40] = min_marker
+    # data2[-40:-1, -40:-1] = max_marker
+    # data3[0:40, 0:40] = min_marker
+    # data3[-40:-1, -40:-1] = max_marker
 
     # Współrzędne środka przecięcia [90, 90, 120]
     x_center, y_center, z_center = 90, 90, 120
@@ -87,7 +87,6 @@ def chart3d_vispy(data1=None, data2=None, data3=None, min_heat=None, max_heat=No
 
 
 def get_frame(filename, name):
-    filename = os.path.join('compare_cubes', filename)
     cube = load_from_file(filename)
     cube['mu_a'] = 0.37
     cube['name'] = name
@@ -130,14 +129,155 @@ def get_min_max_heat(arr_lists):
 
 
 
+class experiment():
+    def __init__(self, category, experiment_name, benchmark_path, folders):
+        self.category = category #
+        self.experiment_name = experiment_name #
+
+        self.folders = folders #
+        self.filenames = [] #
+        self.benchmark_path = benchmark_path #
+        self.mu_a = [] #
+        self.all_cubes_names = [] #
+        self.params_types = [] #
+
+        self.cubes_path = 'CUBES'
+
+        self.file_paths = []
+
+
+    def get_all_cubes_names_from_filenames(self):
+        self.all_cubes_names = []
+        for f_list in self.filenames:
+            this_dir_cube_names = []
+            for fn in f_list:
+                cube_name = fn[:-5]
+                this_dir_cube_names.append(cube_name)
+            self.all_cubes_names.append(this_dir_cube_names)
+
+
+    def file_paths_from_filenames(self):
+        self.file_paths = []
+        for dir_id in range(len(self.folders)):
+            this_folder_paths = []
+            for fn_id in range(len(self.filenames[dir_id])):
+                # path = '/'.join([self.cubes_path, self.folders[dir_id], self.filenames[dir_id][fn_id]])
+                path = os.path.join(self.cubes_path, self.folders[dir_id], self.filenames[dir_id][fn_id])
+                this_folder_paths.append(path)
+            self.file_paths.append(this_folder_paths)
+
+
+    def make_folders_long(self):
+        self.folders_long = []
+        for dir_id in range(len(self.folders)):
+            this_folder = []
+            for _ in range(len(self.filenames[dir_id])):
+                this_folder.append(self.folders[dir_id])
+            self.folders_long.append(this_folder)
+
+
+    def get_filenames_from_folders(self):
+        self.filenames = []
+        for f in self.folders:
+            path = os.path.join(self.cubes_path, f)
+            file_list = os.listdir(path)
+            self.filenames.append(file_list)
+        self.file_paths_from_filenames()
+        self.make_folders_long()
+
+
+    def mua_per_dir(self, mua_list):
+        self.mu_a = []
+        for mua, fn in zip(mua_list, self.filenames):
+            self.mu_a.append([mua] * len(fn)) # łączenie tablic
+
+
+    def params_types_per_dir(self, params_types):
+        self.params_types = []
+        for pt, fn in zip(params_types, self.filenames):
+            self.params_types.append([pt] * len(fn))
+        # print(self.params_types)
+
+
+    def mua_from_file(self):
+        self.mu_a = []
+        for dir_id in range(len(self.folders)):
+            this_folder_mua = []
+            for p in self.file_paths[dir_id]:
+                with open(p, 'r') as f:
+                    cub = json.load(f)
+                mua = cub['mu_a']
+                this_folder_mua.append(mua)
+            self.mu_a.append(this_folder_mua)
+
+
+    def run(self):
+
+        # przypisz właściwości podstawowe
+        all_cubes_names = sum(self.all_cubes_names, []) # łączenie tablic
+        all_mu_a = sum(self.mu_a, [])
+        all_filename_list = sum(self.file_paths, [])
+        all_dir_list = sum(self.folders_long, [])
+        all_path_list = sum(self.file_paths, [])
+        params_types = sum(self.params_types, [])
+
+        arr_lists = []
+        for path, name in zip(all_path_list, all_cubes_names):
+            arr_lists.append(get_frame(path, name))
+
+        # min_heat, max_heat = get_min_max_heat(arr_lists)
+        min_heat, max_heat = None, None
+
+        for al in arr_lists:
+            show3d_cube(al, min_heat=min_heat, max_heat=max_heat)
+
+        
+
+
+
+
+
 if __name__ == '__main__':
-    # chart3d_vispy()
 
-    arr_lists = []
-    arr_lists.append(get_frame("mati_1mln_cube.json", "mati_1mln_cube"))
-    arr_lists.append(get_frame("mc456_mc_100mln_my_params_cube.json", "benchmark_my_100mln_cube"))
 
-    min_heat, max_heat = get_min_max_heat(arr_lists)
 
-    for al in arr_lists:
-        show3d_cube(al, min_heat=min_heat, max_heat=max_heat)
+
+
+    test = experiment(category = 'specjalistyczne mc456 my-params light-sources',
+                  experiment_name = '',
+                  benchmark_path = ['CUBES', 'mc456 my-params light-sources', 'mc456_mc_100mln_my_params_tiss_id_4_g_0_9_ls_down_cube.json'],
+                  folders = ['mc456 my-params light-sources'],
+                  )
+    test.get_filenames_from_folders()
+    test.get_all_cubes_names_from_filenames()
+    test.mua_per_dir([0.37])
+    test.params_types_per_dir(['my'])
+    test.run()
+
+    # mc456 my-params light-sources
+
+
+
+
+    # test = experiment(category = 'specjalistyczne mc456 rozne_skóry_z_tabeli (8 rodzajów) 100mln',
+    #               experiment_name = '',
+    #               benchmark_path = ['CUBES', 'mc456 rozne_skóry_z_tabeli (8 rodzajów) 100mln', 'mc456_mc_100mln_my_params_tiss_id_4_cube.json'],
+    #               folders = ['mc456 rozne_skóry_z_tabeli (8 rodzajów) 100mln'],
+    #               )
+    # test.get_filenames_from_folders()
+    # test.get_all_cubes_names_from_filenames()
+    # test.mua_from_file()
+    # test.params_types_per_dir(['my'])
+    # test.run()
+
+    # mc456 rozne_skóry_z_tabeli (8 rodzajów) 100mln
+
+
+
+
+
+
+
+
+
+
